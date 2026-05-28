f7deacd7d4aa3138aaff7049c1aa64c57953fee0
Selection070 <d.dvoeryadkin@yandex.ru>
2026-05-28 17:43:19 +1000

    add post film

--- api/api_v1/films/views.py
+++ api/api_v1/films/views.py
@@ -1,8 +1,15 @@
 from typing import Annotated
 
+from annotated_types import (
+    MinLen,
+    MaxLen,
+)
+
 from fastapi import (
     Depends,
     APIRouter,
+    Form,
+    status,
 )
 
 from api.api_v1.films.dependencies import get_film_by_id
@@ -25,3 +32,21 @@ async def get_film(
     film: Annotated[Film, Depends(get_film_by_id)],
 ):
     return film
+
+
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_film(
+    film_id: Annotated[int, Form()],
+    title: Annotated[str, MinLen(min_length=3), MaxLen(max_length=20), Form()],
+    description: Annotated[str, MaxLen(max_length=200), Form()],
+    author: Annotated[str, MinLen(min_length=3), MaxLen(max_length=30), Form()],
+) -> Film:
+    return Film(
+        id=film_id,
+        title=title,
+        description=description,
+        author=author,
+    )
