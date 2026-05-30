781cdee4162e31153fd342b06c25b9fdf80ee248
Selection070 <d.dvoeryadkin@yandex.ru>
2026-05-30 12:28:14 +1000

    Refactor api, delete id add slug

--- api/api_v1/films/views.py
+++ api/api_v1/films/views.py
@@ -1,17 +1,19 @@
 from typing import Annotated
 
-from random import randint
-
-
 from fastapi import (
     Depends,
     APIRouter,
     status,
 )
 
-from api.api_v1.films.dependencies import get_film_by_id
+from api.api_v1.films.dependencies import get_film_by_slug
+
 from api.api_v1.films.crud import FILMS_LIST
-from schemas.films import Film, FilmCreate
+
+from schemas.films import (
+    Film,
+    FilmCreate,
+)
 
 router = APIRouter(
     prefix="/films",
@@ -19,14 +21,14 @@ router = APIRouter(
 )
 
 
-@router.get("/films")
+@router.get("/")
 async def get_films():
     return {"films": FILMS_LIST}
 
 
-@router.get("/films/{film_id}")
+@router.get("/{slug}")
 async def get_film(
-    film: Annotated[Film, Depends(get_film_by_id)],
+    film: Annotated[Film, Depends(get_film_by_slug)],
 ):
     return film
 
@@ -39,6 +41,5 @@ async def create_film(
     new_film: FilmCreate,
 ) -> Film:
     return Film(
-        id=randint(2, 1000),
         **new_film.model_dump(),
     )
