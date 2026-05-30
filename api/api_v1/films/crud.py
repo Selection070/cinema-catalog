7ac10e8512cdaefed71567fce379ef81ca876aea
Selection070 <d.dvoeryadkin@yandex.ru>
2026-05-30 12:29:09 +1000

    Refactor films list, delete id add slug

--- api/api_v1/films/crud.py
+++ api/api_v1/films/crud.py
@@ -1,9 +1,14 @@
 from schemas.films import Film
 
 FILMS_LIST = [
-    Film(id=1, title="Fight club", description="...", author="Chuck Palahniuk"),
     Film(
-        id=2,
+        slug="FC",
+        title="Fight club",
+        description="...",
+        author="Chuck Palahniuk",
+    ),
+    Film(
+        slug="BR2049",
         title="Blade Runner 2049",
         description="Rayan Gosling",
         author="Hampton Fancher and Michael Green",
