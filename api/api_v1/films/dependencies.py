4e6fc3c1eb4ba03e7b29f03cd356c6f7d689ad37
Selection070 <d.dvoeryadkin@yandex.ru>
2026-05-30 12:27:59 +1000

    Refactor dependencies, delete id add slug

--- api/api_v1/films/dependencies.py
+++ api/api_v1/films/dependencies.py
@@ -5,15 +5,15 @@ from api.api_v1.films.crud import FILMS_LIST
 from schemas.films import Film
 
 
-async def get_film_by_id(mediad_id: int) -> Film:
-    finded_film: Film | None = next(
-        (film for film in FILMS_LIST if film.id == mediad_id),
+async def get_film_by_slug(slug: str) -> Film:
+    film: Film | None = next(
+        (film for film in FILMS_LIST if film.slug == slug),
         None,
     )
-    if finded_film:
-        return finded_film
+    if film:
+        return film
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Film by id {mediad_id!r} not found",
+        detail=f"Film by id {slug!r} not found",
     )
