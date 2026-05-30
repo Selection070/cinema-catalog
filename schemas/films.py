4d2b30bb5c293a05eb81bf3dacc3f688d24c2c5a
Selection070 <d.dvoeryadkin@yandex.ru>
2026-05-30 12:22:30 +1000

    Add ne schema FilmCreate

--- schemas/films.py
+++ schemas/films.py
@@ -1,11 +1,38 @@
-from pydantic import BaseModel
+from typing import Annotated
+
+from annotated_types import (
+    MinLen,
+    MaxLen,
+)
+
+from pydantic import (
+    BaseModel,
+    Field,
+)
 
 
 class FilmBase(BaseModel):
-    id: int
+    id: int = Field(...)
     title: str
     description: str
     author: str
+
+
+class FilmCreate(BaseModel):
+    title: Annotated[
+        str,
+        MinLen(min_length=3),
+        MaxLen(max_length=20),
+    ]
+    description: Annotated[
+        str,
+        MaxLen(max_length=200),
+    ]
+    author: Annotated[
+        str,
+        MinLen(min_length=3),
+        MaxLen(max_length=30),
+    ]
 
 
 class Film(FilmBase):
